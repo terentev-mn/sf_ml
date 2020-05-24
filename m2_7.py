87dd4245969a690e0c0d83a00ca6c5dea29e70b9
Terentev Maksim <terentev.mn@gmail.com>
2020-05-24 13:24:30 +0300

    go

--- m2_7.py
+++ m2_7.py
@@ -2,12 +2,31 @@
 
 import pandas as pd
 import numpy as np
+from sklearn.preprocessing import StandardScaler
 
-# Задание 2.5.6
-# Загрузите данные train.csv, найдите признак, обозначающий штат.
-# Затем найдите значение, которое встречается чаще всего.
-# Замените пропуски этим значением и запишите получившийся признак в переменную result.
+# Задание 2.7.3
+# Загрузите данные train.csv, найдите признак, обозначающий баланс.
+# Нормализуйте признак с помощью std-нормализации.
+# Проверьте, что вы нашли нужный признак и нормализовали его подходящим методом.
+# Метод для нормализации принимает матрицу, а не массив.
+# В numpy можно превратить массив в матрицу с помощью reshape().
+# В качестве ответа укажите минимальное значение в получившемся нормализованном признаке.
+# Ответ округлите до 5 знаков после запятой.
+
 vis_data = pd.read_csv("train.csv", encoding = 'ISO-8859-1', low_memory = False)
+# print(vis_data.info()) # находим столбец 'balance_due'
+# берем столбец 'balance_due' (получаем массив)
+balance = vis_data.balance_due.values
+# размер массива
+balance_size = balance.size
+# преобразуем массив в матрицу
+bal_matrix = balance.reshape(balance_size, 1)
+
+scaler = StandardScaler()
+balance_norm = scaler.fit_transform(bal_matrix)
+balnce_norm_min = balance_norm.min()
+print('balnce_norm_min', round(balnce_norm_min, 5))
+'''
 # исследуем столбец 'state'
 # вариант №1
 print(vis_data.state.value_counts(dropna=False))
@@ -16,3 +35,4 @@ mode_state = vis_data.state.mode()
 print('значение, которое встречается чаще всего в "state"', mode_state)
 # тут надо извлечь из dtype часть 'MI'
 result = vis_data.state.fillna('MI')
+'''
