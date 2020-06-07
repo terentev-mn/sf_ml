a5b7735b8ca54132458396369546930c84380ef4
Terentev Maksim <terentev.mn@gmail.com>
2020-06-07 13:24:35 +0300

    2.9

--- m2_9.py
+++ m2_9.py
@@ -3,6 +3,9 @@
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import StandardScaler
+import seaborn as sns # библиотека для визуализации статистических данных
+import matplotlib.pyplot as plt # для построения графиков
+
 
 # Задание 2.9.3
 # Загрузите данные train.csv, найдите признак, обозначающий баланс.
@@ -14,8 +17,25 @@ from sklearn.preprocessing import StandardScaler
 vis_data = pd.read_csv("train.csv", encoding = 'ISO-8859-1', low_memory = False)
 # print(vis_data.info()) # находим столбец 'balance_due'
 # берем столбец 'balance_due' (получаем массив)
-balance = vis_data.balance_due.values
+#balance = vis_data.balance_due.values
+#plt.figure(figsize=(16,6))
+a = np.sqrt(vis_data.balance_due[vis_data.balance_due > 0])
+print('median', np.median(a))
+print('mean', np.mean(a))
+print('ABS', np.abs(np.median(a) - np.mean(a)))
+#a = np.log(vis_data.balance_due[vis_data.balance_due > 0]).hist()
+#print(a)
+#plt.show()
+
+
+#b = np.abs(a)
+#print(b)
 '''
+plt.figure(figsize=(16,6))
+heat_map1 = sns.heatmap(data=re1_corr, annot=True)
+#heat_map0 = sns.heatmap(data=re0_corr, annot=True)
+plt.show()
+
 # размер массива
 balance_size = balance.size
 # преобразуем массив в матрицу
